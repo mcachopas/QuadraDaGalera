8d0a5e2d895a47a69256bae3428af40cd9c11d70
Daniel-Geovane-Gomes <danielgeovanegomes@gmail.com>
2024-02-06 23:11:47 -0300

    Commit Daniel - Classes
    Classes iniciais feitas

--- QuadraDaGaleraApi/Dominio/Entidades/Classes.cs
+++ QuadraDaGaleraApi/Dominio/Entidades/Classes.cs
@@ -1,1 +1,46 @@
 ﻿// Na sequencia crie as classes aqui
+
+using System.Diagnostics.Contracts;
+
+public class Usuario
+{
+    public String name { get; set; }
+    public String email { get; set; }
+    public DateTime DateBirth { get; set; }
+    public String telephone { get; set; }
+    public bool isWhatsApp { get; set; }
+
+}
+
+public class Estabelecimento
+{
+    public String name { get; set; }
+    public String? cnpj { get; set; }
+    public String adress { get; set; }
+    public DateTime OpeningTime { get; set;}
+    public DateTime CloseTime { get; set; }
+
+}
+
+public class Quadra
+{
+    public String name { get; set; }
+    public bool nightLighting {  get; set; }
+    public String surfacetType {  get; set; }
+    public bool isCovered {  get; set; }
+    public int capacity {  get; set; }
+    public string size { get; set; }
+}
+
+public class Partida
+{
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public TimeSpan Duration { get { return HoraInicio - HoraTermino } }
+    public List<Usuario> Players { get; set; }
+
+    public Partida()
+    {
+        Players = new List<Usuario>();
+    }
+}
