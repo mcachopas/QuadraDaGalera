f9fb3e6da8ad9a7f7cb0b6f43aa49e9317ae6c61
Daniel-Geovane-Gomes <115510177+Daniel-Geovane-Gomes@users.noreply.github.com>
2024-02-13 20:05:26 -0300

    Correção de erro Classes.cs
    Correção do erro no nome das propriedades e do fechamento da classe 'Endereco'.

--- QuadraDaGaleraApi/Dominio/Entidades/Classes.cs
+++ QuadraDaGaleraApi/Dominio/Entidades/Classes.cs
@@ -1,1 +1,56 @@
-﻿// Na sequencia crie as classes aqui
+// Na sequencia crie as classes aqui
+
+using System.Diagnostics.Contracts;
+
+public class Usuario
+{
+    public String name { get; set; }
+    public String email { get; set; }
+    public DateTime DateBirth { get; set; }
+    public String telephone { get; set; }
+    public bool isWhatsApp { get; set; }
+
+}
+
+public class Estabelecimento
+{
+    public String name { get; set; }
+    public String? cnpj { get; set; }
+    public String adress { get; set; }
+    public DateTime OpeningTime { get; set;}
+    public DateTime CloseTime { get; set; }
+
+}
+
+public class Endereco
+{
+    public int cep { get; set; }
+    public String state { get; set; }
+    public String city { get; set; }
+    public String neighborhood { get; set; }
+    public String street { get; set; }
+    public int number { get; set; }
+}
+
+    public class Quadra
+    {
+        public String name { get; set; }
+        public bool nightLighting { get; set; }
+        public String surfacetType { get; set; }
+        public bool isCovered { get; set; }
+        public int capacity { get; set; }
+        public string size { get; set; }
+    }
+
+    public class Partida
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration { get { return StartTime - EndTime; } }
+        public List<Usuario> Players { get; set; }
+
+        public Partida()
+        {
+            Players = new List<Usuario>();
+        }
+    }
