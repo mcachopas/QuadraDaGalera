360f33a36395a935fb29d2a755caa4071fdc8032
Mooses Cachopas <mcachopas@gmail.com>
2024-02-15 21:29:53 -0300

    Commit Altercaos Controller
    Commit Altercaos Controller

---
+++ QuadraDaGaleraApi/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuadraDaGaleraApi.Dominio.Dtos;
+
+namespace QuadraDaGaleraApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Usuário retornado com sucesso");
+        }
+
+        [HttpPut]
+        [Route("ChangeDateOfBirth")]
+        public IActionResult ChangeDateOfBirth(DateOfBirthUpdateDto dateOfBirthUpdateDto)
+        {
+            return Ok("Data de nascimento alterada com sucesso");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            return Ok("Usuário deletado com sucesso");
+        }
+    }
+}
