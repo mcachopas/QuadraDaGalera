abc7c38d52d0f9f18d7cb836d8bc17e11f0ea02c
Mooses Cachopas <mcachopas@gmail.com>
2024-02-20 21:38:17 -0300

    Commit Teste DB
    Commit Teste DB

--- QuadraDaGaleraApi/infra/QuadraDaGaleraDbContext.cs
+++ QuadraDaGaleraApi/infra/QuadraDaGaleraDbContext.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using QuadraDaGaleraApi.Dominio.Entidades;
 
 namespace QuadraDaGaleraApi.infra
 {
     public class QuadraDaGaleraDbContext(DbContextOptions<QuadraDaGaleraDbContext> options) : DbContext(options)
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Establishment> Establishments { get; set; }
+        public DbSet<Field> Fields { get; set; }
+        public DbSet<Match> Matches { get; set; }
     }
 }
