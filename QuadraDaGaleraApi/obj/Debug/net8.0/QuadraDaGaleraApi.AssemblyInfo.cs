85582d9a376bf7b3dfa91979dc226dbabdd18f82
Daniel-Geovane-Gomes <danielgeovanegomes@gmail.com>
2024-02-20 21:50:17 -0300

    Correção no nome da classe
    Correção no nome da classe

--- QuadraDaGaleraApi/obj/Debug/net8.0/QuadraDaGaleraApi.AssemblyInfo.cs
+++ QuadraDaGaleraApi/obj/Debug/net8.0/QuadraDaGaleraApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuadraDaGaleraApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8161e95b8c7ceb5d893009df9746b3da279ce3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89cb10446595d4d66f1f5826ad4784e997b53fab")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuadraDaGaleraApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuadraDaGaleraApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
